03ccc50fc8273b38420e4b6afdbe186db6b21b67
박지은 <62520045+krapeun@users.noreply.github.com>
2020-08-02 02:55:56 +0900

    Create 2446.cpp

---
+++ 2000/2446.cpp
@@ -0,0 +1,26 @@
+// 2446 별 찍기 - 9
+
+#include <stdio.h>
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	for (int i = N; i > 1; i--) {
+		for (int j = 1; j <= N - i; j++)
+			printf(" ");
+		for (int j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N - i; j++)
+			printf(" ");
+		for (int j = 1; j <= 2 * i - 1; j++)
+			printf("*");
+		printf("\n");
+	}
+
+	return 0;
+}
