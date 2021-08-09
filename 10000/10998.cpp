e4c5192efeeae3551864f22f3c7c90014b20fb3a
박지은 <62520045+krapeun@users.noreply.github.com>
2021-08-09 23:17:34 +0900

    Create 10998.cpp

---
+++ 10000/10998.cpp
@@ -0,0 +1,17 @@
+// #10998 AxB
+
+#include <stdio.h>
+
+int main(){
+    int A, B;
+    scanf("%d %d", &A, &B);
+    
+    if(A < 0) A = 1;
+    else if(A > 10) A = 9;
+    if(B < 0) B = 1;
+    else if(B > 10) B = 9;
+    
+    printf("%d\n", A * B);
+    
+    return 0;
+}
